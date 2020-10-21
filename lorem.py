31890e9a7569254f6499d2af0d69b1979045ada1
wuaoxiang <pylint@yandex.com>
2020-10-21 10:51:14 +0800

    Add no_std

--- lorem.py
+++ lorem.py
@@ -1,3 +1,4 @@
+# Only support Linux
 import datetime
 import os
 
@@ -6,29 +7,30 @@ def modify():
     file = open('lorem.md', 'r')
     flag = int(file.readline()) == 0
     file.close()
-    file = open('lorem.md', 'w+')
-    if flag:
-        file.write('1')
-    else:
-        file.write('0')
-        file.close()
+    with open('lorem.md', 'w+') as f:
+        f.write('1' if flag else '0')
 
 
 def commit():
     os.system('git commit -a -m update_lorem > /dev/null 2>&1')
 
 
-def set_sys_time(year, month, day):
+def set_sys_time(year: int, month: int, day: int):
+    """
+    `date -s` command only support in Linux os
+    to reset system datetime: use ntp server or
+    date +"%d %b %Y %T %Z" -s "$(wget -qSO- --max-redirect=0 https://baidu.com 2>&1 | grep '^  Date:' | cut -d' ' -f 5-)"
+    """
     os.system('date -s %04d%02d%02d' % (year, month, day))
 
 
-def trick_commit(year, month, day):
+def trick_commit(year: int, month: int, day: int):
     set_sys_time(year, month, day)
     modify()
     commit()
 
 
-def daily_commit(start_date, end_date):
+def daily_commit(start_date: datetime.date, end_date: datetime.date):
     for i in range((end_date - start_date).days + 1):
         cur_date = start_date + datetime.timedelta(days=i)
         trick_commit(cur_date.year, cur_date.month, cur_date.day)
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+// #![feature(once_cell)]
+#![no_std]
+// use core::ptr::null;
+// use core::lazy::Lazy;
+
+// struct String;
+
 #[cfg(test)]
 mod tests {
     #[test]
